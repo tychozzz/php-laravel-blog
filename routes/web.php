00e999ed4af1a399ea283d8ea932cd6da7e6911a
ltyzzz <ltyzzz2000@gmail.com>
2023-05-27 17:56:02 +0800

    feat:代码生成器 && 生成topic相关代码

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 // 个人中心
 Route::resource("users", "UsersController", ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
