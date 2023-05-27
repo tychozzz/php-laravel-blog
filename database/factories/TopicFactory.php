2946a51205ef69fd387b091fade092d00561d71e
ltyzzz <ltyzzz2000@gmail.com>
2023-05-27 18:14:04 +0800

    feat: 用户话题假数据填充

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -11,8 +11,13 @@ class TopicFactory extends Factory
 
     public function definition()
     {
+        $sentence = fake()->sentence();
         return [
-            // $this->faker->name,
+            'title' => $sentence,
+            'body' => fake()->text(),
+            'excerpt' => $sentence,
+            'user_id' => fake()->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]),
+            'category_id' => fake()->randomElement([1, 2, 3, 4]),
         ];
     }
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(10)->create();
+
+        $user = User::find(1);
+        $user->name = 'ltyzzz';
+        $user->email = 'ltyzzz@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
