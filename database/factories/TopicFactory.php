9d082a0b121ef8d49d204ccb53e593905b78b5e7
ltyzzz <ltyzzz2000@gmail.com>
2023-05-27 18:41:24 +0800

    perf: 引入debug工具 修复N+1问题 优化性能

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -16,7 +16,7 @@ class TopicFactory extends Factory
             'title' => $sentence,
             'body' => fake()->text(),
             'excerpt' => $sentence,
-            'user_id' => fake()->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]),
+            'user_id' => fake()->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
             'category_id' => fake()->randomElement([1, 2, 3, 4]),
         ];
     }
