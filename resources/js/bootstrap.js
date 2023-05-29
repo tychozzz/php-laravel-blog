0d8bd56def7a1e860c4491ff5b69623fdadf0ac7
ltyzzz <ltyzzz2000@gmail.com>
2023-05-29 13:49:49 +0800

    feat: 引入markdown编辑器

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,10 @@
 window._ = require('lodash');
 
 try {
-    require('bootstrap');
-} catch (e) {}
+  window.$ = window.jQuery = require('jquery');
+  require('bootstrap');
+} catch (e) {
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -30,7 +30,7 @@
 
                   <div class="mb-3">
                     <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title) }}"
-                           placeholder="请填写标题" required />
+                           placeholder="请填写标题" required/>
                   </div>
 
                   <div class="mb-3">
@@ -48,7 +48,8 @@
                   </div>
 
                   <div class="well well-sm">
-                    <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i> 保存</button>
+                    <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i> 保存
+                    </button>
                   </div>
                 </form>
         </div>
@@ -56,3 +57,22 @@
     </div>
   </div>
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
